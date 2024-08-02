86ad39797f3655637971c0c9830d98a664a62447
ManiacUrgency <leonard.jin.0707@gmail.com>
2024-08-01 22:30:22 -0700

    added stopwords removal

--- test_bm25_stopwords.py
+++ test_bm25_stopwords.py
@@ -1,7 +1,12 @@
-from rank_bm25 import BM25Okapi
-from nltk.tokenize import word_tokenize
 import nltk
-nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from rank_bm25 import BM25Okapi
+
+# Load stop words
+stop_words = set(stopwords.words('english'))
 
 # Sample documents
 documents = [
@@ -10,15 +15,18 @@ documents = [
     "The cat climbed the tree",
 ]
 
-# Tokenize the documents
-tokenized_docs = [word_tokenize(doc.lower()) for doc in documents]
+# Tokenize and remove stop words
+tokenized_docs = [
+    [word for word in word_tokenize(doc.lower()) if word.isalnum() and word not in stop_words]
+    for doc in documents
+]
 
 # Initialize the BM25 model
 bm25 = BM25Okapi(tokenized_docs)
 
 # Sample query
 query = "cat on the mat"
-tokenized_query = word_tokenize(query.lower())
+tokenized_query = [word for word in word_tokenize(query.lower()) if word.isalnum() and word not in stop_words]
 
 # Get BM25 scores
 scores = bm25.get_scores(tokenized_query)
